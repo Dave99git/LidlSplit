d70ee31fa1449bf161665eac106e2d2d97957b86
Dave99git <david.fuhrmann.0107@gmail.com>
2025-06-13 11:16:12 +0200

    Implement purchases screen with RecyclerView

--- app/src/main/java/de/th/nuernberg/bme/lidlsplit/MainActivity.java
+++ app/src/main/java/de/th/nuernberg/bme/lidlsplit/MainActivity.java
@@ -1,12 +1,22 @@
 package de.th.nuernberg.bme.lidlsplit;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +30,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerPurchases);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        PurchaseAdapter adapter = new PurchaseAdapter(createDummyPurchases());
+        recyclerView.setAdapter(adapter);
+
+        Button addButton = findViewById(R.id.btnAddPurchase);
+        addButton.setOnClickListener(v ->
+                Toast.makeText(this, "+ Einkauf hinzufügen", Toast.LENGTH_SHORT).show());
+
+        ImageButton filterButton = findViewById(R.id.btnFilter);
+        filterButton.setOnClickListener(v ->
+                Toast.makeText(this, "Filter", Toast.LENGTH_SHORT).show());
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> true);
+    }
+
+    private List<Purchase> createDummyPurchases() {
+        List<Purchase> purchases = new ArrayList<>();
+        purchases.add(new Purchase("29.03.2025", "54,99€", false));
+        purchases.add(new Purchase("30.03.2025", "31,20€", true));
+        purchases.add(new Purchase("31.03.2025", "9,49€", false));
+        return purchases;
     }
 }
