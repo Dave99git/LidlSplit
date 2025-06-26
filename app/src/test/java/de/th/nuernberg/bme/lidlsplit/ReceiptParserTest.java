1a7470a307568f7218321c337783f0ed2a0f67c5
Dave99git <david.fuhrmann.0107@gmail.com>
2025-06-26 15:45:41 +0200

    Handle price advantage lines

---
+++ app/src/test/java/de/th/nuernberg/bme/lidlsplit/ReceiptParserTest.java
@@ -0,0 +1,34 @@
+package de.th.nuernberg.bme.lidlsplit;
+
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.*;
+
+public class ReceiptParserTest {
+    @Test
+    public void parseSampleReceipt() {
+        String text = "Allersberger Straße 130\n" +
+                "90461 Nürnberg\n" +
+                "Cherrystrauchtomaten 1,79 A\n" +
+                "Preisvorteil -0,20\n" +
+                "Laugenbrezel 10er          1,99 A\n" +
+                "---\n" +
+                "zu zahlen             19,86\n" +
+                "18.06.2025";
+
+        ReceiptParser parser = new ReceiptParser();
+        ReceiptData data = parser.parse(text);
+
+        assertEquals("Allersberger Straße 130", data.getStreet());
+        assertEquals("90461 Nürnberg", data.getCity());
+        assertEquals(2, data.getItems().size());
+        assertEquals("Cherrystrauchtomaten", data.getItems().get(0).getName());
+        assertEquals(1.59, data.getItems().get(0).getPrice(), 0.001);
+        assertEquals("Laugenbrezel 10er", data.getItems().get(1).getName());
+        assertEquals(1.99, data.getItems().get(1).getPrice(), 0.001);
+        assertEquals(19.86, data.getTotal(), 0.001);
+        assertEquals(LocalDateTime.of(2025, 6, 18, 0, 0), data.getDateTime());
+    }
+}
