1246285b64a80af5eb70e3bec0d78d18950222ec
Dave99git <david.fuhrmann.0107@gmail.com>
2025-06-26 19:11:13 +0200

    Replace receipt parsing and add Article model

--- app/src/main/java/de/th/nuernberg/bme/lidlsplit/ParsedReceipt.java
+++ app/src/main/java/de/th/nuernberg/bme/lidlsplit/ParsedReceipt.java
@@ -3,19 +3,19 @@ package de.th.nuernberg.bme.lidlsplit;
 import java.util.List;
 
 public class ParsedReceipt {
-    private final List<Artikel> items;
+    private final List<Article> items;
     private final String date;
     private final double total;
     private final String address;
 
-    public ParsedReceipt(List<Artikel> items, String date, double total, String address) {
+    public ParsedReceipt(List<Article> items, String date, double total, String address) {
         this.items = items;
         this.date = date;
         this.total = total;
         this.address = address;
     }
 
-    public List<Artikel> getItems() {
+    public List<Article> getItems() {
         return items;
     }
 
