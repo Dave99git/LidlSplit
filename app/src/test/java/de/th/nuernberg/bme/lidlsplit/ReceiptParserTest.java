ea1818bb031f9dd11bf39d1346bf94140c6e72da
Dave99git <david.fuhrmann.0107@gmail.com>
2025-06-26 20:40:50 +0200

    Handle 'Betrag' lines when parsing totals

--- app/src/test/java/de/th/nuernberg/bme/lidlsplit/ReceiptParserTest.java
+++ app/src/test/java/de/th/nuernberg/bme/lidlsplit/ReceiptParserTest.java
@@ -58,4 +58,19 @@ public class ReceiptParserTest {
         assertEquals(1.99, data.getItems().get(1).getPrice(), 0.001);
         assertEquals(19.86, data.getTotal(), 0.001);
     }
+
+    @Test
+    public void parseTotalWithBetrag() {
+        String text = "Allersberger Straße 130\n" +
+                "90461 Nürnberg\n" +
+                "Betrag 19,86\n" +
+                "18.06.2025";
+
+        ReceiptParser parser = new ReceiptParser();
+        ReceiptData data = parser.parse(text);
+
+        assertEquals(19.86, data.getTotal(), 0.001);
+        assertEquals("Allersberger Straße 130", data.getStreet());
+        assertEquals("90461 Nürnberg", data.getCity());
+    }
 }
